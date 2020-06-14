1ca8b1cff20fe1a8bb344ce4f29f0ee79d659946
hemanth kumar <pusupalahemanthkumar@gmail.com>
2020-06-14 20:54:31 +0530

    done with basic setup of websokets in deno

--- 06_DenoWebSockets/01_WebSockets/server.ts
+++ 06_DenoWebSockets/01_WebSockets/server.ts
@@ -1,11 +1,39 @@
 /* Introduction To Websockets In Deno */
 // Importing Required Files And Packages Here.
 import { serve } from "https://deno.land/std/http/server.ts";
+import {
+  acceptWebSocket,
+  isWebSocketCloseEvent,
+  isWebSocketPingEvent,
+  WebSocket,
+} from "https://deno.land/std/ws/mod.ts";
 
+const Connections =new Array<WebSocket>();
 async function main() {
   console.log("Server is started at http://localhost:8080");
+
+
   for await (const req of serve("localhost:8080")) {
-      req.respond({body: "Hello Websokets!"})
+    const { conn, headers, w:bufWriter, r:bufReader } = req;
+    acceptWebSocket({ conn, headers, bufReader, bufWriter })
+      .then(async (ws: WebSocket): Promise<void> => {
+        console.log("WebSokects Connection Established.");
+        Connections.push(ws);
+        console.log(Connections.length);
+        for await (const event of ws) {
+          if (typeof event === "string") {
+            // console.log(event);
+            // ws.send(event);
+            for(const websocket of Connections){
+                websocket.send(event);
+            }
+          }
+
+          if (isWebSocketCloseEvent(event)) {
+            console.log("WebSokets Connection Closed!");
+          }
+        }
+      });
   }
 }
 main();
